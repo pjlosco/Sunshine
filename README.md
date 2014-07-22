a5aa93c9ff64a491f50fbdb30df16f7c4b4d2887
Patrick Losco <plosco@shutterstock.com>
2014-07-22 11:23:31 -0400

    Added detail activity with fragment. Just started on settings

--- README.md
+++ README.md
@@ -8,10 +8,14 @@ Sunshine App for android
 
 1. Setup new project with package name. Might need to be a "module" since that gives full options to customize
 2. Project should be a blank activity with a fragment
-
-(fill in other stuff that happened here)
-
-X1. Create a refresh button in the options menu
+3. Min SDK is what is the minimum supported OS, target is what should be used to test on.
+4. Try not to use AbsoluteLayout when creating UI, and keep it responsive
+5. Using an adapter populate a list and insert into the UI using ListView on an XML
+6. Created AsyncTask class for getting data
+ - override the doInBackground() method to get and analyze the data. Must be used over a network
+ - override the onPostExecute() to take in the result of doInBackground and interact with the main thread
+ - use AsyncTask.execute() to start the task
+7. Create a refresh button in the options menu
  - added a new xml menu (see forecastfragment.xml)
  - created a string for the name of the option in strings.xml
     <string name="action_refresh">Refresh</string>
@@ -19,4 +23,20 @@ X1. Create a refresh button in the options menu
     onCreate()
     onCreateOptionsMenu() - actually inflate it here
     onOptionsItemSelected() - set references and create the action when selected
-X2. Add a permission for internet access - TODO
+8. Add a permission for internet access
+ - <uses-permission android:name="android.permission.INTERNET"/>
+9. Create JSON manager/parser
+10. Update local data from JSON
+11. Create setOnItemClickListener statements within onCreateView to have actions when an item in the list is clicked
+12. Using a Toast is an easy way to get started and confirm its acting right
+13. Using an Intent is the way to connect to other Activities.
+14. Create a new activity by generating files within Android Studio, new Blank Activity with Fragment. This will update the manifest automatically
+15. Connect intent with Activity
+ - Intent intent = new Intent(getActivity(),DetailActivity.class).putExtra(Intent.EXTRA_TEXT, "Something useful");
+ - startActivity(intent);
+16. Create layout for new activity. res > layout > activity_detail.xml, fragment_detail.xml
+17. Update intent with useful data to populate into new activity
+18. Update new activity with importing the intent during onCreateView and apply to an object that was added in the XML
+ - String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+ - ((TextView) rootView.findViewById(R.id.detail_text)).setText(forecastStr);
+19.
--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -4,19 +4,31 @@ package com.example.android.sunshine.app;
  * Created by patricklosco on 7/17/14.
  */
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -32,15 +44,24 @@ public class ForecastFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        String[] exampleList = {"Today - Sunny - 88/63","Today - Sunny - 88/63","Today - Sunny - 88/63"};
-        ArrayList<String> weekForecast = new ArrayList<String>(Arrays.asList(exampleList));
+        new FetchWeatherTask().execute("10314");
 
-        mForecastAdapter = new ArrayAdapter<String>(this.getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
+        mForecastAdapter = new ArrayAdapter<String>(this.getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview);
 
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forcast);
         listView.setAdapter(mForecastAdapter);
 
-        new FetchWeatherTask(null);
+        new FetchWeatherTask().execute("10314");
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String forecast = mForecastAdapter.getItem(position);
+                Intent intent = new Intent(getActivity(),DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(intent);
+            }
+        });
 
         return rootView;
     }
@@ -70,4 +91,95 @@ public class ForecastFragment extends Fragment {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+
+        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+
+        @Override
+        protected String[] doInBackground(String... params) {
+            if (params.length == 0) {
+                return null;
+            }
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            String forecastJsonStr = null;
+            int numDays = 7;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                final String FORECAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily";
+
+                Uri uri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                        .appendQueryParameter("q", params[0])
+                        .appendQueryParameter("mode", "json")
+                        .appendQueryParameter("units", "metric")
+                        .appendQueryParameter("cnt", numDays+"")
+                        .build();
+
+                URL url = new URL(uri.toString());
+                Log.v(LOG_TAG, "Built URI" + uri.toString());
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+                Log.v(LOG_TAG, "Forecast JSON string " + forecastJsonStr);
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                return null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            try {
+                return new WeatherDataParser().getWeatherDataFromJson(forecastJsonStr, numDays);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String[] result) {
+            if (result != null) {
+                mForecastAdapter.clear();
+                mForecastAdapter.addAll(result);
+            }
+        }
+    }
+
 }
